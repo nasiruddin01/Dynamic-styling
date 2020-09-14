3f157983c73ab58968c032faafac027cafe60862
nasiruddin01 <nasiruddin.dev@gmail.com>
2020-09-15 05:57:15 +0600

    master

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 new Vue({
     el: '#app',
     data: {
-        attachRed: false
+        attachRed: false,
+        color: 'green'
+    },
+    computed: {
+        divClasses: function () {
+            return {
+                red: this.attachRed,
+                blue: !this.attachRed
+            };
+        }
     }
 
-    });
+});
